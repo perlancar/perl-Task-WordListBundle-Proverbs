89dd132c4316c958e7490d3facf0010cc31e427e
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-07-22 07:11:43 +0700

    Initial commit

---
+++ lib/Task/WordListBundle/Proverbs.pm
@@ -0,0 +1,17 @@
+package Task::WordListBundle::Proverbs;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: All proverbs WordList modules (in all languages)
+
+=pkggroup Included modules
+
+#CODE: for (`lcpan mods --namespace WordList -n Proverb`) { print "=pkg $_\n" }
+
+=cut
